de76562c81966b61859f006072e2411804a3fe2e
Sasa M <smikodanic@gmail.com>
2018-07-18 15:27:47 +0200

    disable/enable drag-scroll on certain area

--- src/app/pages/page1/page1.component.ts
+++ src/app/pages/page1/page1.component.ts
@@ -9,9 +9,26 @@ import { Component } from '@angular/core';
 export class Page1Component {
 
   title: string;
+  disableDrag_tf: boolean;
 
   constructor() {
     this.title = 'Page1';
+    this.disableDrag_tf = false;
+  }
+
+
+  // disable drag
+  disableDrag(evt) {
+    this.disableDrag_tf = true;
+    console.log('Disable drag: ', this.disableDrag_tf);
+    evt.preventDefault();
+    evt.stopPropagation();
+  }
+
+  // enable drag
+  enableDrag() {
+    this.disableDrag_tf = false;
+    console.log('Enable drag: ', this.disableDrag_tf);
   }
 
 }
